041e5cd12a8ca9aeed68d971dd4efca86d8f0407
nghianguyen982000 <nghia.nguyen@pionero.io>
2022-11-30 09:29:27 +0700

    update react-hook-form

--- client/src/Pages/Login/index.tsx
+++ client/src/Pages/Login/index.tsx
@@ -1,14 +1,26 @@
 import BackgroundAuth from "../../Assets/img/backgroundAuth.jpg";
 import imgLogin from "../../Assets/img/login.png";
 import { Link } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
-const schemaLogin = yup.object().shape({
-  user_name: yup.string().required(),
-  password: yup.string().required().min(6),
+const schemaLogin = yup.object().shape({  
+  user_name: yup.string().required("Please fill in this field"),
+  password: yup.string().required("Please fill in this field").min(6),
 });
 
+interface LoginProps{
+  user_name: string;
+  password: string;
+}
 const Login = () => {
+  const { register, handleSubmit ,formState: { errors }} = useForm<LoginProps>({
+    resolver: yupResolver(schemaLogin)
+  });
+  const onSubmit = (data: LoginProps) => {
+    console.log(data)
+  };
   return (
     <div
       className="bg-top bg-cover bg-no-repeat h-screen"
@@ -25,22 +37,26 @@ const Login = () => {
         </div>
         <div className="flex-[50%]">
           <div className="text-[35px] font-bold mb-[15px]">LOGIN</div>
-          <form className="flex flex-col">
+          <form className="flex flex-col"onSubmit={handleSubmit(onSubmit)}>
             <input
               type="text"
               className="h-[40px] outline-transparent "
               placeholder="User name"
+              {...register('user_name')}
             />
             <hr className="mb-[15px]" />
+            {errors.user_name && <p className="text-[red] text-[12px]">{errors.user_name.message}</p>}
             <input
+            {...register('password')}
               type="password"
-              className="h-[40px] outline-transparent"
+              className="h-[40px] outline-transparent"  
               placeholder="Password"
             />
-            <hr className="mb-[30px]" />
+            <hr className="mb-[15px]" />
+            {errors.password && <p className="text-[red] text-[12px]">{errors.password.message}</p>}
             <button
               type="submit"
-              className="h-[35px] w-[100px] bg-[#1890ff] text-[#fff] "
+              className="h-[35px] w-[100px] bg-[#1890ff] text-[#fff] mt-[15px]"
             >
               Login
             </button>
