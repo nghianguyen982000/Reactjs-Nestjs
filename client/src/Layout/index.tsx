62acc8830826a8b04893420f765539c2001e1b23
nghianguyen982000 <nghia.nguyen@pionero.io>
2022-12-06 06:59:30 +0700

    update

--- client/src/Layout/index.tsx
+++ client/src/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '../Components/Header'
+import DetailCourse from '../Pages/DetailCourse'
 import Home from '../Pages/Home'
 
 const Layout = () => {
@@ -9,6 +10,7 @@ const Layout = () => {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/detail' element={<DetailCourse/>}></Route>
       </Routes>
     </>
   )
